d91157b0eefc71d006cb0e09d3a4156784307792
praveen-hb <praveenudu9@gmail.com>
2020-04-12 17:28:38 +0530

    created a sign up form for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,14 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
   def index
-    render plain: User.order(:id).map { |user| user.to_pleasant_string }.join("\n")
-  end
-
-  def show
-    render plain: User.find(params[:id]).to_pleasant_string
+    render "new"
   end
 
   def create
-    name = params[:name]
-    email = params[:email]
-    password = params[:password]
-    new_user = User.create!(
-      name: name,
-      email: email,
-      password: password,
+    User.create!(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
     )
-    response_text = "New user created with id #{new_user.id}"
-    render plain: response_text
-  end
-
-  def login
-    render plain: User.where(email: params[:email], password: params[:password]).exists?
-  end
-
-  def update
-    id = params[:id]
-    user = User.find(id)
-    user.password = params[:password]
-    user.save!
-    render plain: "password for #{user.id} updated"
+    redirect_to "/"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #get "todos", to: "todos#index"
-  #post "todos", to: "todos#create"
-  #get "todos/:id", to: "todos#show"
+  get "/" => "home#index"
   resources :todos
   resources :users
   post "users/login", to: "users#login"
